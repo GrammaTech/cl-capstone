cb3661fe3f6dffcd1d98236d7c566a27ba27cf63
Eric Schulte <eschulte@grammatech.com>
2020-03-17 09:48:16 -0400

    Use a static-vector to hold the code for disasm

--- capstone.lisp
+++ capstone.lisp
@@ -1,5 +1,6 @@
 (defpackage :capstone
   (:use :gt :cffi)
+  (:import-from :static-vectors :with-static-vector)
   (:export :disasm
            ;; Fields of the instruction object.
            :address :mnemonic :opstr))
@@ -248,7 +249,7 @@ this instruction with cs_free(insn, 1)"
 
 (defcfun "cs_free" :void
   "Free memory allocated by cs_malloc() or cs_disasm() (argument @insn)
-    
+
 @insn: pointer returned by @insn argument in cs_disasm() or cs_malloc()
 @count: number of cs_insn structures returned by cs_disasm(), or 1
         to free memory allocated by cs_malloc()."
@@ -279,18 +280,20 @@ this instruction with cs_free(insn, 1)"
 
 ;;;; Test.
 (defun test-disasm ()
-  (let ((handle (foreign-alloc 'capstone-handle))
-        (instr* (foreign-alloc '(:pointer (:struct capstone-instruction))))
-        (bytes (foreign-alloc :uint8 :count 8)))
-    (assert (eql :ok (cs-open :x86 :64 handle)))
-    (let ((code #(#x55 #x48 #x8b #x05 #xb8 #x13 #x00 #x00)))
-      (dotimes (n 8) (setf (mem-aref bytes :uint8 n) (aref code n))))
-    (format t "Handle: ~x:~x~%" handle (mem-ref handle 'capstone-handle))
-    (format t "Disassembly:~%")
-    (let ((count (cs-disasm (mem-ref handle 'capstone-handle) bytes 7 #x1000 0 instr*)))
-      (assert (and (numberp count) (> count 0)))
-      (dotimes (n count)
-        (with-foreign-slots ((address mnemonic op_str)
-                             (mem-aref instr* :pointer n)
-                             (:struct capstone-instruction))
-          (format t "~x: ~a ~a~%" address mnemonic op_str))))))
+  (with-static-vector (code 8 :element-type '(unsigned-byte 8) :initial-contents
+                            '(#x55 #x48 #x8b #x05 #xb8 #x13 #x00 #x00))
+    (let ((handle (foreign-alloc 'capstone-handle))
+          (instr* (foreign-alloc '(:pointer (:struct capstone-instruction)))))
+      (assert (eql :ok (cs-open :x86 :64 handle)))
+      (format t "Handle: ~x:~x~%" handle (mem-ref handle 'capstone-handle))
+      (format t "Disassembly:~%")
+      ;; NOTE: Memory fault at the location held in the memory pointed
+      ;;       to by the HANDLE pointer.
+      (let ((count (cs-disasm (mem-ref handle 'capstone-handle)
+                              bytes 7 #x1000 0 instr*)))
+        (assert (and (numberp count) (> count 0)))
+        (dotimes (n count)
+          (with-foreign-slots ((address mnemonic op_str)
+                               (mem-aref instr* :pointer n)
+                               (:struct capstone-instruction))
+            (format t "~x: ~a ~a~%" address mnemonic op_str)))))))
